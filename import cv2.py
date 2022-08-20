2191cba275757a2a71a8bdc5e66c02d0ebcbb6b7
Ishan Srivastava <63843789+ishan-sriv-17@users.noreply.github.com>
2022-08-20 21:32:10 +0200

    metric added

--- import cv2.py
+++
@@ -1,8 +0,0 @@
-import cv2
-
-image = cv2.imread(r'C:/Users/ishan/Desktop/Image Fusion/Fusion_R1/train_samples/count_3000.png')
-
-cv2.imshow('Original', image)
-cv2.waitKey(0)
-
-print(image.shape)
